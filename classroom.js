6d12737eb83006054731dcf0f7f3c41157fd6432
alvarocalace@hotmail.com <alvarocalace@hotmail.com>
2018-08-17 21:47:50 -0300

    eslint

--- classroom.js
+++ classroom.js
@@ -1,47 +1,47 @@
-(function(window) {
+(function (window) {
 	"use strict";
-   
+
 	function customizeLinks(links, kanjiInfo) {
 		function handler(event) {
 			event.preventDefault();
 			kanjiInfo.src = event.target.href;
 		}
-        
+
 		for (let link of links) {
 			link.addEventListener("click", handler);
 		}
-        
+
 		if (links.length > 0) {
 			links[links.length - 1].click();
 		}
 	}
-    
+
 	function maybeUseFallbackSource(kanjiInfo, defaultSource, fallbackSource) {
 		const src = kanjiInfo.src;
-        
-		if (src.startsWith(defaultSource)) {     
+
+		if (src.startsWith(defaultSource)) {
 			fetch(src).then(response => {
 				if (response.status !== 200) {
 					kanjiInfo.src = src.replace(defaultSource, fallbackSource);
 				}
-			}); 
+			});
 		}
 	}
-    
-	function addRtkLinks(text, defaultSource) {		
+
+	function addRtkLinks(text, defaultSource) {
 		return text.replace(/([\u4e00-\u9faf])/g, `<a href='${defaultSource}$1'>$1</a>`);
 	}
-	
+
 	function cleanText(text) {
 		const filters = [
-			{exp: /\u3000/g, rep: ""},  // weird line breaks
-			{exp: /】[^「]+「/g, rep: "】「"} // file paths
+			{ exp: /\u3000/g, rep: "" },  // weird line breaks
+			{ exp: /】[^「]+「/g, rep: "】「" } // file paths
 		];
-		
+
 		for (const filter of filters) {
 			text = text.replace(filter.exp, filter.rep);
 		}
-		
+
 		return text;
 	}
 
@@ -87,10 +87,10 @@
 			return accum + `<dd>${key}</dd><dt>${data[key]}</dt>`;
 		}, "");
 	}
-    
+
 	function onMutation(records, kanjiInfo, insertTarget, stats, defaultSource) {
 		const target = records[0].target;
-		
+
 		records.forEach(record => record.addedNodes.forEach(node => {
 			node.title = insertTarget.childNodes.length;
 			node.innerHTML = addRtkLinks(cleanText(node.textContent), defaultSource) + "\n";
@@ -99,29 +99,29 @@
 		updateStats(stats, insertTarget);
 		target.scrollTop = target.scrollTopMax;
 	}
-    
+
 	function clearElement(element) {
 		element.innerHTML = "";
 	}
-    
+
 	function initInsertTargetObserver(insertTarget, kanjiInfo, stats, defaultSource) {
 		new MutationObserver(mutation => onMutation(mutation, kanjiInfo, insertTarget, stats, defaultSource))
-			.observe(insertTarget, {childList: true});
+			.observe(insertTarget, { childList: true });
 	}
-    
+
 	function initClearButton(clear, insertTarget) {
 		clear.addEventListener("click", () => clearElement(insertTarget));
 	}
-    
+
 	function initKanjiInfo(kanjiInfo, defaultSource, fallbackSource) {
 		if (fallbackSource) {
 			kanjiInfo.addEventListener("load", () => maybeUseFallbackSource(kanjiInfo, defaultSource, fallbackSource));
 		}
-		
+
 		kanjiInfo.src = defaultSource;
 	}
-    
-	function initBackground(background, count, template, tries = 3) {     
+
+	function initBackground(background, count, template, tries = 3) {
 		if (count > 0 && tries > 0) {
 			background.onerror = () => initBackground(background, count, template, tries - 1);
 			background.src = template(Math.floor(Math.random() * count) + 1);
@@ -129,7 +129,7 @@
 			background.remove();
 		}
 	}
-    
+
 	function initKeybinds(background, backgroundCount, backgroundTemplate) {
 		const handlers = {
 			r: event => {
@@ -141,7 +141,7 @@
 
 		document.addEventListener("keydown", event => {
 			const key = event.key.toLowerCase();
-            
+
 			if (handlers[key]) {
 				handlers[key](event);
 			}
@@ -161,7 +161,7 @@
 			if (delimiter.test(value)) {
 				textarea.value = "";
 
-				if (value.length > 1)  {
+				if (value.length > 1) {
 					const node = document.createElement("p");
 					node.textContent = value.replace(delimiter, "");
 					insertTarget.appendChild(node);
@@ -169,7 +169,7 @@
 			}
 		});
 	}
-    
+
 	function initClassroom(dom, config) {
 		initInsertTargetObserver(dom.insertTarget, dom.kanjiInfo, dom.stats, config.defaultSource);
 		initClearButton(dom.clear, dom.insertTarget);
